ab06b8607fdc654c72c0c8bb4021a2e5ce72d99d
Jan Rosell <janrosellcaba@gmail.com>
2026-08-06 22:29:04 +0000

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,25 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
-import vercel from '@astrojs/vercel';
+import node from '@astrojs/node';
 import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
 
-  output: 'static',
+  output: 'server',
 
-  adapter: vercel(),
+  adapter: node({
+    mode: 'standalone',
+  }),
+  
+  vite: {
+    preview: {
+      allowedHosts: true,
+    },
+    server: {
+      allowedHosts: true,
+    }
+  }
 });
